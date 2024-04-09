b4afa91acaf5c0ab0bc30da017795ad4d2964608
Josh221josh <Teyilliongroup006@gmail.com>
2024-04-08 18:27:45 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 
-const lang1 = "C is fun";
-const lang2 = "Python is cool";
-const lang3 = "JavaScript is amazing";
-
-console.log(lang1);
-console.log(lang2);
-console.log(lang3);
+console.log("C is fun\nPython is cool\nJavaScript is amazing");
